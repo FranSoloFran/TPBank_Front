dd81a0263b6451f0cb45cc7e74b62d726ee6ba95
FranSoloFran <franciscomgallardo@icloud.com>
2020-11-14 19:33:41 -0300

    searchclient

--- components/SearchClientOperation/SearchClientOperation.js
+++ components/SearchClientOperation/SearchClientOperation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 
 const clientes = axios.create({
     baseURL: 'https://bank-api-integrations.herokuapp.com/api/v1/clients/'
@@ -120,10 +121,7 @@ class SearchClientOperation extends Component {
                     <div className='client-info'>
                         <div className='row'>
                             <div className='col-md-6'> 
-                            <p><b>Nombre:</b> {this.state.firstname}</p>
-                            </div>
-                            <div className='col-md-6'>
-                                <p><b>Apellido:</b> {this.state.lastname}</p>
+                            <h4>{this.state.lastname}, {this.state.firstname}</h4>
                             </div>
                         </div>
 
@@ -131,30 +129,64 @@ class SearchClientOperation extends Component {
                             <div className='col-md-6'>
                                 <p><b>Email:</b> {this.state.email}</p>
                             </div>
-                            <div className='col-md-6'>
+                            {/* <div className='col-md-6'>
                             <p><b>Estado:</b>Activo</p>
+                            </div> */}
+                        </div>
+                        <div className="pricing-area pt-70 pb-50">
+                        <div className="container">
+                        <div className="row">
+                        {this.state.accounts.map(account => (
+                        <div className="col-lg-4 col-md-6" key={account.id}>
+                            <div className="single-pricing-box">
+                                <div className="pricing-header">
+                                    <h3>Cuenta n° {account.id}</h3>
+                                    <p>{this.state.firstname} {this.state.lastname}</p>
+                                </div>
+
+                                <div className="price">
+                                    <span className="symbol">$</span> {account.balance} <span>balance</span>
+                                </div>
+ 
+                                <ul className="pricing-features">
+                                    <li>
+                                        Tipo de cuenta: {account.account_type_description} 
+                                        {/* <i className="fas fa-check"></i>  */}
+                                    </li>
+                                    <li>
+                                        Número de cuenta: {account.identification_number} 
+                                        {/* <i className="fas fa-check"></i>  */}
+                                    </li>
+                                    <li>
+                                        Balance: {account.balance} 
+                                        {/* <i className="fas fa-check"></i>  */}
+                                    </li>
+                                    <li>
+                                        Alias: {account.name} 
+                                        {/* <i className="fas fa-ban"></i>  */}
+                                    </li>
+                                    <li>
+                                        Descubierto: {account.overdraft} 
+                                        {/* <i className="fas fa-ban"></i>  */}
+                                    </li>
+                                    <li>
+                                        Activa: {account.active} 
+                                        {/* <i className="fas fa-ban"></i>  */}
+                                    </li>
+                                </ul>
+
+                                <div className="buy-btn">
+                                    <Link href="#">
+                                        <a className="btn btn-primary">Editar </a>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                        {this.state.accounts.map(account => (
-                            <div className='row' key={account.id}>
-                                <div className='col-md-12'>
-                                    <br></br>
-                                    <p><b>Cuenta número :</b> {account.id}</p>
-                                </div>
-                                <div className='col-md-6'>
-                                    <p><b>Tipo de cuenta:</b> {account.account_type}</p>
-                                </div>
-                                <div className='col-md-6'>
-                                    <p><b>Número de cuenta:</b> {account.identification_number}</p>
-                                </div>
-                                <div className='col-md-6'>
-                                    <p><b>Balance:</b> {account.balance}</p>
-                                </div>
-                                <div className='col-md-6'>
-                                    <p><b>Nombre:</b> {account.name}</p>
-                                </div>
-                            </div>
+                        
                         ))}
+                        </div>
+                        </div>
+                        </div>
 
                     </div>
 
